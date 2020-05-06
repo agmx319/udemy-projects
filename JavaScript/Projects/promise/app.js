0dd44d408d0a5192b361a6a4f10f4ffc5309699d
gmx <a.319@gmx.com>
2020-05-06 15:32:28 +1000

    added promise

--- JavaScript/Projects/promise/app.js
+++ JavaScript/Projects/promise/app.js
@@ -3,11 +3,19 @@ const posts = [
   {title: 'Post Two' , body: 'This is post two'}
 ];
 
-function createPost(post, callback) {
-  setTimeout(function() {
-    posts.push(post);
-    callback(); 
-  }, 2000);
+function createPost(post) {
+  return new Promise(function(resolve, reject){
+    setTimeout(function() {
+      posts.push(post);
+      const err = false;
+      if(!err) {
+        resolve();
+      } else {
+        reject('Error: Something went wrong!!');
+      }
+      
+    }, 2000);
+  });
 }
 
 function getPosts() {
@@ -20,4 +28,8 @@ function getPosts() {
   }, 500)
 }
 
-createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
+createPost({title: 'Post Three', body: 'This is post three'})
+.then(getPosts)
+.catch(function(err) {
+  console.log(err);
+})

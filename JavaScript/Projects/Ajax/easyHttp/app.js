69e7b713a70036a3121c83d4a82722eb22a83487
gmx <a.319@gmx.com>
2020-05-06 14:21:23 +1000

    Delete method added

--- JavaScript/Projects/Ajax/easyHttp/app.js
+++ JavaScript/Projects/Ajax/easyHttp/app.js
@@ -1,11 +1,58 @@
 const http = new easyHTTP;
 
-http.get('https://jsonplaceholder.typicode.com/posts',
-function(err, posts) {
+// Get all posts
+// http.get('https://jsonplaceholder.typicode.com/posts',
+// function(err, posts) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+  
+// });
+
+//Get single post
+// http.get('https://jsonplaceholder.typicode.com/posts/1',
+// function(err, posts) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+  
+// });
+
+// Create Data
+const data = {
+  title: 'Custom post',
+  body: 'This is a custom post'
+};
+
+//Create Post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+//Put
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+//Delete
+http.delete('https://jsonplaceholder.typicode.com/posts/1',
+function(err, response) {
   if(err) {
     console.log(err);
   } else {
-    console.log(posts);
+    console.log(response);
   }
   
 });
